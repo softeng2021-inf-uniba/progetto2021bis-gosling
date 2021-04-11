23f83482cde354a843b70b2562fbe829a351d2fb
Pasquale <p.monniello@studenti.uniba.it>
2021-04-11 19:05:45 +0200

    Aggiunta esecuzione dell'help

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 
 import it.uniba.sotorrent.GoogleDocsUtils;
+import java.util.Scanner;
 
 /**
  * The main class for the project. It must be customized to meet the project
@@ -14,7 +15,7 @@ import it.uniba.sotorrent.GoogleDocsUtils;
  * <b>DO NOT RENAME</b>
  */
 public final class AppMain {
-
+        
 	/**
 	 * Private constructor. Change if needed.
 	 */
@@ -28,32 +29,55 @@ public final class AppMain {
 	 * @param args The command-line arguments.
 	 */
 	public static void main(final String[] args) {
-
-		System.out.println("Current working dir: " + System.getProperty("user.dir"));
-
+                
+		System.out.println("Benvenuto nell'applicazione di Dama!");
+                
 		if (args.length > 0) {
-			switch (args[0]) {
-			case "it":
-				System.out.println("Applicazione avviata.");
-				break;
-
-			case "en":
-				System.out.println("Application started.");
+                    for(String s: args)
+                    {
+                        switch (args[0]) {
+			case "help":
+			case "h":
+				System.out.println("Eseguo comando Help");
 				break;
 
 			default:
-				System.out.println("Specify the language. "
-								   + "Languages supported: 'it' or 'en'");
 				break;
 			}
-		} else {
-			System.out.println("Using default language 'en'");
-			System.out.println("Application started.");
+                    }
 		}
-            Damiera dam = Damiera.getDamiera();
-            dam.stampaNumeri();
-            System.out.print("\n\n\n\n");
-            dam.stampaPedine();
+                System.out.println("Per sapere quali comandi sono validi digitare help");
+                boolean isExiting = false; 
+                String answer;
+                Scanner sc = new Scanner(System.in);
+                do
+                {
+                    System.out.println("Digitare un comando valido...");
+                    if(sc.hasNextLine())
+                    {
+                        answer = sc.nextLine();
+                        switch(answer)
+                        {
+                            case "help":
+                                Help.getMenuHelp();
+                                break;
+                            case "numeri":
+                                Damiera.getDamiera().stampaNumeri();
+                                break;
+                            case "damiera":
+                                System.out.println("Per eseguire questo comando devi essere in una partita!");
+                                System.out.println("Per iniziare una partita esegui il comando gioca");
+                                break;
+                            //va inserito gioca
+                            // va inserito esci
+                            default:
+                                System.out.println("Comando inserito non valido");
+                                System.out.println("Per sapere quali comandi sono validi digitare help");
+                                break;
+                        } 
+                    }          
+                }while(isExiting==false);
+                sc.close();
 	}
 
 }
