79dddc6585820a3d20b4b6c6d9b79365688dae66
MaxT-21 <mtubito21@gmail.com>
2021-04-25 19:23:21 +0200

    Implementato spostamento di pedina con coversione dell'input senza controlli

--- src/main/java/it/uniba/main/Damiera.java
+++ src/main/java/it/uniba/main/Damiera.java
@@ -1,6 +1,7 @@
 package it.uniba.main;
 
 import it.uniba.main.types.Colore;
+import it.uniba.main.types.Posizione;
 
 /**
  *
@@ -13,14 +14,16 @@ public class Damiera {
     private static Damiera singleIstance; // Per rendere la classe singleton
     private final int[][] damieraNumeri;
     private final Pedina[][] damieraGioco;
-    
+    private final Posizione[] vettorePosizioni;
     
     /* ------------  Costruttori ------------ */
     private Damiera()
     {
         damieraGioco = new Pedina[8][8];
         damieraNumeri = new int[8][8];
+        vettorePosizioni = new Posizione[32];
         int counter = 1;
+        preparaVettore();
         preparaDamiera();
         for(int i=0; i<8; i++)
         {
@@ -54,6 +57,40 @@ public class Damiera {
         }
     }
     
+    private void preparaVettore(){
+        vettorePosizioni[0] = new Posizione(0,0);
+        vettorePosizioni[1] = new Posizione(0,2);
+        vettorePosizioni[2] = new Posizione(0,4);
+        vettorePosizioni[3] = new Posizione(0,6);
+        vettorePosizioni[4] = new Posizione(1,1);
+        vettorePosizioni[5] = new Posizione(1,3);
+        vettorePosizioni[6] = new Posizione(1,5);
+        vettorePosizioni[7] = new Posizione(1,7);
+        vettorePosizioni[8] = new Posizione(2,0);
+        vettorePosizioni[9] = new Posizione(2,2);
+        vettorePosizioni[10] = new Posizione(2,4);
+        vettorePosizioni[11] = new Posizione(2,6);
+        vettorePosizioni[12] = new Posizione(3,1);
+        vettorePosizioni[13] = new Posizione(3,3);
+        vettorePosizioni[14] = new Posizione(3,5);
+        vettorePosizioni[15] = new Posizione(3,7);
+        vettorePosizioni[16] = new Posizione(4,0);
+        vettorePosizioni[17] = new Posizione(4,2);
+        vettorePosizioni[18] = new Posizione(4,4);
+        vettorePosizioni[19] = new Posizione(4,6);
+        vettorePosizioni[20] = new Posizione(5,1);
+        vettorePosizioni[21] = new Posizione(5,3);
+        vettorePosizioni[22] = new Posizione(5,5);
+        vettorePosizioni[23] = new Posizione(5,7);
+        vettorePosizioni[24] = new Posizione(6,0);
+        vettorePosizioni[25] = new Posizione(6,2);
+        vettorePosizioni[26] = new Posizione(6,4);
+        vettorePosizioni[27] = new Posizione(6,6);
+        vettorePosizioni[28] = new Posizione(7,1);
+        vettorePosizioni[29] = new Posizione(7,3);
+        vettorePosizioni[30] = new Posizione(7,5);
+        vettorePosizioni[31] = new Posizione(7,7);
+    }
     /* ------------ Get & Set ------------*/
     
     public static Damiera getDamiera() // Serve ad ottenere damiera
@@ -168,5 +205,25 @@ public class Damiera {
             System.out.println("|");
             System.out.println("+---+---+---+---+---+---+---+---+");
         }
+    }
+    
+    public Posizione convertiPosizione(int pos){
+        return this.vettorePosizioni[pos-1];
+    }
+    
+    public boolean spostaPedina(String answer){
+        boolean corretta = false;
+        String[] numeri = answer.split("-");
+        int numPartenza = Integer.valueOf(numeri[0]);
+        int numArrivo = Integer.valueOf(numeri[1]);
+        Posizione posPartenza = convertiPosizione(numPartenza);
+        Posizione posArrivo = convertiPosizione(numArrivo);
+        //inserire i controolli per eseguire lo spostamento
+        corretta=true;
+        if(corretta){
+            this.damieraGioco[posArrivo.riga][posArrivo.colonna] = this.damieraGioco[posPartenza.riga][posPartenza.colonna];
+            this.damieraGioco[posPartenza.riga][posPartenza.colonna] = null;
+        }
+        return corretta;
     }
 }
---
+++ src/main/java/it/uniba/main/types/Posizione.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.uniba.main.types;
+
+/**
+ *
+ * @author User
+ */
+public class Posizione {
+    public int riga;
+    public int colonna;
+
+    public Posizione(int riga, int colonna) {
+        this.riga = riga;
+        this.colonna = colonna;
+    }
+}
